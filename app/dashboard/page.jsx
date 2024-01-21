7f249ea1a2660f1845a5369a5fe5baabab480fc7
Omar Cordoba <53019698+raamas@users.noreply.github.com>
2024-01-21 10:22:31 -0500

    Update page.jsx

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -8,7 +8,7 @@ import Workout from "@components/Workout.jsx";
 import { useWorkoutsStore } from "@utils/stores";
 import { getServerSession } from "next-auth"
 import { redirect } from "next/navigation"
-import { authOptions } from "@app/api/auth/[...nextauth]/route.js
+import { authOptions } from "@app/api/auth/[...nextauth]/route.js"
 
 
 async function Dashboard() {
