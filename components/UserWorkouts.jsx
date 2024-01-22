57b8424599ed628d6b98df75956b51b97a8105a3
Omar Cordoba <53019698+raamas@users.noreply.github.com>
2024-01-21 21:28:05 -0500

    Update UserWorkouts.jsx

--- components/UserWorkouts.jsx
+++ components/UserWorkouts.jsx
@@ -1,1 +1,32 @@
 "use client"
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
+import Workout from "@components/Workout.jsx";
+import { useWorkoutsStore } from "@utils/stores";
+import { v4 } from "uuid";
+
+function UserWorkouts(){
+  const { data: session } = useSession();
+  let workouts = useWorkoutsStore((state) => state.workoutsState);
+  const getUserWorkouts = useWorkoutsStore((state) => state.getUserWorkouts);
+
+
+  useEffect(() => {
+    const invokeGetUserWorkouts = () => {
+      getUserWorkouts({
+        id: session.user.id,
+        name: session.user.name,
+      });
+    };
+
+    invokeGetUserWorkouts();
+  }, []);
+
+return(
+  <div className="text-base-content">
+          {workouts.map((workout) => {
+            return <Workout workout={workout} key={v4()} />;
+          })}
+        </div>
+)
+}
