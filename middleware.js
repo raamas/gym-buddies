e02cc241407b8eaacdab30fafb20901dd8dee0d7
ommars <omardenilson007@gmail.com>
2024-01-21 20:44:50 -0500

    Fixing major production errors related to session

--- middleware.js
+++ middleware.js
@@ -1,8 +1,8 @@
-import { withAuth } from 'next-auth/middleware'
+export { default } from 'next-auth/middleware'
 
-export default withAuth({
-    secret: process.env.NEXTAUTH_SECRET
-})
+// export default withAuth({
+//     secret: process.env.NEXTAUTH_SECRET
+// })
 
 export const config = {
     matcher: ['/dashboard/:path*', '/workouts/:path*/edit', '/workouts/create']
