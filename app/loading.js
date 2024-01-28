de2a0f0a8241eed881404b9d482ac6b9f663e330
Omar <omardenilson007@gmail.com>
2024-01-27 20:00:30 -0500

    added loading.js

---
+++ app/loading.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Loading() {
+  return (
+    <div className="min-w-screen min-h-[90vh] flex flex-col items-center justify-center bg-base-100">
+        <span className="loading loading-spinner text-primary"></span>
+
+    </div>
+  )
+}
+
+export default Loading
