2ce60f458270adfcaac3abc46a462d8f90963c05
Omar Cordoba <53019698+raamas@users.noreply.github.com>
2024-07-24 10:55:46 -0500

    Update Workout.jsx

--- components/Feed/Workout.jsx
+++ components/Feed/Workout.jsx
@@ -26,7 +26,7 @@ function Workout({ workout }) {
 					<></>
 				);
 			})}
-			<span className="flex flex-start gap-3">
+			<span className="flex flex-start self-end">
 				<p className="mb-4 text-neutral-content font-light">
 					{workout.userCount?.[0].count} saves
 				</p>
